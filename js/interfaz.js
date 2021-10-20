ed10eedb41503975bdf0a78d5d79c83e61eb673f
crisfreak <cristopher.sotog@utem.cl>
2021-10-20 16:58:16 -0300

    Add selectors to export

---
+++ js/interfaz.js
@@ -0,0 +1,7 @@
+export const contenedorBusqueda = document.querySelector('#buscar'),
+            formulario = document.querySelector('#formulario-buscar'),            
+            inputArtista = document.querySelector('#artista'),            
+            inputCancion = document.querySelector('#cancion'),            
+            encabezadoResultado = document.querySelector('.letra-resultado h2'),            
+            resultado = document.querySelector('#resultado'),            
+            contenedorMensajes = document.querySelector('#mensajes');            
