2a646b8fcf32ede50ff6fef174484ee60309016d
crisfreak <cristopher.sotog@utem.cl>
2021-10-20 17:33:25 -0300

    Add function to show an alert

--- js/interfaz.js
+++ js/interfaz.js
@@ -4,4 +4,16 @@ export const contenedorBusqueda = document.querySelector('#buscar'),
             inputCancion = document.querySelector('#cancion'),            
             encabezadoResultado = document.querySelector('.letra-resultado h2'),            
             resultado = document.querySelector('#resultado'),            
-            contenedorMensajes = document.querySelector('#mensajes');            
+            contenedorMensajes = document.querySelector('#mensajes');
+
+export function mostrarAlerta(mensaje, tipo){
+    if(tipo === 'error'){
+        contenedorMensajes.classList.add('error');
+        contenedorMensajes.textContent = mensaje;
+
+        setTimeout(() => {
+            contenedorMensajes.textContent = '';
+            contenedorMensajes.classList.remove('error');
+        }, 2500);        
+    }
+}
